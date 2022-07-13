870a3681254f6860f8dc18d934613c5be306f5fe
megha kumari <megha kumari@LAPTOP-7JJE1ESH>
2022-07-13 12:06:09 +0530

    foodie-1

---
+++ Foodie-App-04/src/main/java/com/springboot/foodieapp/controller/FoodItemController.java
@@ -0,0 +1,63 @@
+package com.springboot.foodieapp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.foodieapp.model.FoodItem;
+import com.springboot.foodieapp.model.ItemCategory;
+import com.springboot.foodieapp.service.FoodItemService;
+
+@RestController
+public class FoodItemController {
+	
+	@Autowired
+	private FoodItemService service;
+
+	@GetMapping("/items/{item-code}")
+	public FoodItem getFoodItem(@PathVariable("item-code") long itemCode) {
+		return service.findFoodItem(itemCode);
+	}
+	
+	@PostMapping("/items")
+	public FoodItem addFoodItem(@RequestBody FoodItem item) {
+		return service.saveFoodItem(item);
+	}
+	
+	@PutMapping("/items")
+	public FoodItem updateFoodItem(@RequestBody FoodItem item) {
+		return service.updateFoodItem(item);
+	}
+	
+	@DeleteMapping("/items/{item-code}")
+	public boolean deleteFoodItem(@PathVariable("item-code") long itemCode) {
+		return service.deleteFoodItem(itemCode);
+	}
+	
+	@GetMapping("/items")
+	public List<FoodItem> getAllFoodItems(){
+		return service.getAllItems();
+	}
+	
+	@GetMapping("/items/item-name/{item-name}")
+	public FoodItem findFoodItemByName(@PathVariable("item-name") String itemName) {
+		return service.getItemByItemName(itemName);
+	}
+	
+	@GetMapping("/items/category/{category}")
+	public List<FoodItem> findFoodItemsByCategory(@PathVariable ItemCategory category){
+		return service.findItemsByCategory(category);
+	}
+	
+	@GetMapping("/items/price/{price}")
+	public List<FoodItem> findItemInPriceRange(@PathVariable float price){
+		return service.findItemWithinPrice(price);
+	}
+}
---
+++ Foodie-App-04/src/main/java/com/springboot/foodieapp/service/FoodItemServiceImpl.java
@@ -0,0 +1,63 @@
+package com.springboot.foodieapp.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.springboot.foodieapp.model.FoodItem;
+import com.springboot.foodieapp.model.ItemCategory;
+import com.springboot.foodieapp.repository.IFoodItemRepo;
+
+@Service
+public class FoodItemServiceImpl implements FoodItemService{
+
+	@Autowired
+	private IFoodItemRepo repo;
+	
+	@Override
+	@Transactional
+	public FoodItem saveFoodItem(FoodItem item) {
+		return repo.save(item);
+	}
+
+	@Override
+	public FoodItem findFoodItem(long itemCode) {
+		return repo.findById(itemCode).get();
+	}
+
+	@Override
+	@Transactional
+	public FoodItem updateFoodItem(FoodItem item) {
+		return repo.save(item);
+	}
+
+	@Override
+	@Transactional
+	public boolean deleteFoodItem(long itemCode) {
+		 repo.deleteById(itemCode);
+		 return !repo.existsById(itemCode);
+	}
+
+	@Override
+	public List<FoodItem> getAllItems() {
+		return repo.findAll();
+	}
+
+	@Override
+	public FoodItem getItemByItemName(String itemName) {
+		return repo.findByItemName(itemName);
+	}
+	
+	@Override
+	public List<FoodItem> findItemsByCategory(ItemCategory category) {
+		return repo.findByCategory(category);
+	}
+	
+	@Override
+	public List<FoodItem> findItemWithinPrice(float price) {
+		return repo.findItemInPriceRange(price);
+	}
+	
+}
