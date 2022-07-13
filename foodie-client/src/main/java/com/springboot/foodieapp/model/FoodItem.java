e7e2b2d96890ecbaa14f6b8eb7d9ef7e3a38ed0c
megha kumari <megha kumari@LAPTOP-7JJE1ESH>
2022-07-13 12:12:43 +0530

    spring _demo,foodie _client,spring_app

---
+++ foodie-client/src/main/java/com/springboot/foodieapp/model/FoodItem.java
@@ -0,0 +1,16 @@
+package com.springboot.foodieapp.model;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FoodItem {
+	private long itemCode;
+	private String itemName;
+	private float price;
+	private ItemCategory category;
+	private RecipeDetails recipe;
+
+}
